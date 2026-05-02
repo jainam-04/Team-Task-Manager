0dc9ce7d6c8841381956697d84c3bc2e77bf675c
Jainam Rupani <jainamrupani04@gmail.com>
2026-05-02 15:23:57 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl underline text-blue-600 font-bold">Hello World</h1>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_DB_URI)
+      .then(() => console.log("Database connected"))
+      .catch((error) => console.log(error));
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server is running on: http://localhost:${port}`));
